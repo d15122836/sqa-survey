2b96eeba9cd23053f054b825b14bfbb409dbdbf8
d15122836 <d15122836@mydit.ie>
2020-05-07 12:47:57 +0100

    added list Survey function

---
+++ survey/src/test/test_listSurvey.java
@@ -0,0 +1,10 @@
+package test;
+
+import static controller.controller.listSurvey;
+
+public class test_listSurvey {
+    public static void main(String args[])
+    {
+        listSurvey();
+    }
+}
